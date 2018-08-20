4ea8e70211f0438f24077078d98b9689a1fc7e1b
jo <jo@MacBook-Air.local>
2018-08-20 19:21:34 +0900

    add helper.sh

---
+++ src/cmd/preparation-mac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# @TODO: Important!
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew install python3
+
+# brew install pkg-config
+
+# brew install zmq
+# export PKG_CONFIG_PATH=/usr/local/Cellar/zeromq/4.x.x/lib/pkgconfig/
+
+pip3 install -r requirements-mac.txt
